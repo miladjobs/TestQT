8ff5d6b0a1fb25b0ceb66c8f5b24d2041e357986
Milad Esfandiyari <miladisjobs@gmail.com>
2021-03-24 17:04:23 +0430

    add definition of new classes and their slots

--- mainwindow.h
+++ mainwindow.h
@@ -2,10 +2,34 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPushButton>
+#include <QTabBar>
+#include <QTableView>
+#include <QErrorMessage>
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlQuery>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+
+namespace Ui { class MainWindow;}
+
+class PersonModel : public QAbstractTableModel
+{
+    Q_OBJECT
+
+    public:
+        PersonModel(QObject *parent = 0);
+
+        int rowCount(const QModelIndex &parent = QModelIndex()) const Q_DECL_OVERRIDE;
+        int columnCount(const QModelIndex &parent = QModelIndex()) const Q_DECL_OVERRIDE;
+
+        QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const Q_DECL_OVERRIDE;
+        QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const Q_DECL_OVERRIDE;
+
+    private:
+        QList<QString> tm_person_id;
+        QList<QString> tm_person_name;
+
+};
 
 class MainWindow : public QMainWindow
 {
@@ -17,5 +41,12 @@ public:
 
 private:
     Ui::MainWindow *ui;
+private slots:
+    void handleConnectPushButton();
+    void handleSubmitPushButton();
+    void updateTable();
+
+private:
+    QSqlDatabase db;
 };
 #endif // MAINWINDOW_H
